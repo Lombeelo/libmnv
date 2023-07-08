7b9783f874a99266cff76844309de51c6230f533
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2023-07-08 21:15:29 +0300

    Split mnv.hpp to mnv.hpp + mnv-impl.hpp and added mainpage.dox

--- include/mnv/mnv.hpp
+++ include/mnv/mnv.hpp
@@ -1,349 +1,88 @@
 #ifndef MNV_HPP
 #define MNV_HPP
 
-#include <algorithm>
 #include <array>
-#include <cmath>
 #include <mnv/export_static.h>
-#include <random>
 #include <vector>
+
+#include <mnv/mnv-impl.hpp>
+/**
+ * @file mnv.hpp Main interface file of the mnv library
+ * @author Dmitry Parfenyuk (cekunda.rf@gmail.com)
+ * @brief A header-only library to generate random multivariate normal values
+ * @version 0.1
+ * @date 2023-07-08
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 
 namespace mnv
 {
+    /**
+     * @brief Basic vector, can be abstracted away
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam Len Length of array, can be abstracted
+     */
     template <typename T, size_t Len>
     using valueVector = std::array<T, Len>;
 
+    /**
+     * @brief Basic matrix, used only for definitions
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam LenRows Rows count
+     * @tparam LenCols Columns count
+     */
     template <typename T, size_t LenRows, size_t LenCols>
     using Matrix = valueVector<valueVector<T, LenCols>, LenRows>;
 
+    /**
+     * @brief Square matrix, array of arrays, size is statically defined
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam Len Matrix size
+     */
     template <typename T, size_t Len>
     using MatrixSq = Matrix<T, Len, Len>;
 
-    namespace internal
-    {
-        template <typename T, size_t Len>
-        valueVector<T, Len> multiplyVectors(valueVector<T, Len> first, valueVector<T, Len> second)
-        {
-            T result;
-
-            for (int i = 0; i < first.size(); i++)
-            {
-                result += first[i] * second[i];
-            }
-
-            return result;
-        }
-
-        template <typename T, size_t Len>
-        valueVector<T, Len> addVectors(valueVector<T, Len> first, valueVector<T, Len> second)
-        {
-            valueVector<T, Len> result;
-
-            for (int i = 0; i < first.size(); i++)
-            {
-                result[i] = first[i] + second[i];
-            }
-
-            return result;
-        }
-
-        template <typename T>
-        T calculateMean(std::vector<T> &input_values)
-        {
-            T result;
-
-            for (auto &&value : input_values)
-            {
-                result += value;
-            }
-
-            return result / input_values.size();
-        }
-
-        inline bool isOdd(size_t number)
-        {
-            return static_cast<bool>(number & 1);
-        }
-
-        template <typename T, size_t Len>
-        T calculateMinor(MatrixSq<T, Len> matrix, int minor_order)
-        {
-            if (minor_order == 1)
-            {
-                return matrix[0][0];
-            }
-
-            if (minor_order == 2)
-            {
-                return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-            }
-
-            T sum = 0;
-
-            // разложение Лапласа, рекурсивное
-            size_t column = 0;
-            for (size_t row = 0; row < minor_order; row++)
-            {
-                MatrixSq<T, Len> minorMatrix;
-                for (size_t i = 0; i < minor_order - 1; i++)
-                {
-                    for (size_t j = 0; j < minor_order - 1; j++)
-                    {
-                        minorMatrix[i][j] = matrix[i + (column + 1)][j + (j >= row)];
-                    }
-                }
-                T otherMinor = calculateMinor(minorMatrix, minor_order - 1);
-                sum = isOdd(row)
-                          ? sum + otherMinor
-                          : sum - otherMinor;
-            }
-
-            return sum;
-        }
-
-        enum class MatrixDefinition
-        {
-            ZeroDefinite,
-            PositiveDefinite,
-            PositiveSemidefinite,
-            NegativeDefinite,
-            NegativeSemidefinite,
-            Undefinite
-        };
-
-        template <typename T, size_t Len>
-        MatrixDefinition defineMatrix(MatrixSq<T, Len> matrix)
-        {
-            MatrixDefinition result = MatrixDefinition::ZeroDefinite;
-            bool firstZero = false;
-            for (size_t i = 1; i < matrix.size() + 1; i++)
-            {
-                T minor = calculateMinor(matrix, i);
-                if (minor > 0)
-                {
-                    switch (result)
-                    {
-                    case MatrixDefinition::ZeroDefinite:
-                        result = firstZero
-                                     ? MatrixDefinition::PositiveSemidefinite
-                                     : MatrixDefinition::PositiveDefinite;
-                        break;
-
-                    case MatrixDefinition::NegativeDefinite:
-                    case MatrixDefinition::NegativeSemidefinite:
-                        result = MatrixDefinition::Undefinite;
-                        break;
-
-                    default:
-                        break;
-                    }
-                }
-
-                if (minor < 0)
-                {
-                    switch (result)
-                    {
-                    case MatrixDefinition::ZeroDefinite:
-                        result = firstZero
-                                     ? MatrixDefinition::NegativeSemidefinite
-                                     : MatrixDefinition::NegativeDefinite;
-                        break;
-
-                    case MatrixDefinition::PositiveDefinite:
-                    case MatrixDefinition::PositiveSemidefinite:
-                        result = MatrixDefinition::Undefinite;
-                        break;
-
-                    default:
-                        break;
-                    }
-                }
-
-                if (minor == 0)
-                {
-                    switch (result)
-                    {
-                    case MatrixDefinition::PositiveDefinite:
-                        result = MatrixDefinition::PositiveSemidefinite;
-                        break;
-                    case MatrixDefinition::NegativeDefinite:
-                        result = MatrixDefinition::NegativeSemidefinite;
-                        break;
-
-                    case MatrixDefinition::ZeroDefinite:
-                        firstZero = true;
-
-                    default:
-                        break;
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        template <typename T, size_t Len>
-        T sumOfSquaresUntil(valueVector<T, Len> vector, size_t idx)
-        {
-            T sum = 0;
-
-            for (size_t i = 0; i < idx; i++)
-            {
-                sum += vector[i] * vector[i];
-            }
-
-            return sum;
-        }
-
-        template <typename T, size_t Len>
-        T sumOfProductsUntil(valueVector<T, Len> vectorA, valueVector<T, Len> vectorB, size_t idx)
-        {
-            T sum = 0;
-
-            for (size_t i = 0; i < idx; i++)
-            {
-                sum += vectorA[i] * vectorB[i];
-            }
-
-            return sum;
-        }
-
-        template <typename MatrixType>
-        inline bool isMatrixSymmetic(MatrixType matrix)
-        {
-            for (size_t i = 0; i < matrix.size() - 1; i++)
-            {
-                for (size_t j = i + 1; j < matrix.size(); j++)
-                {
-                    if (matrix[i][j] != matrix[j][i])
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        template <typename T, size_t Len>
-        MatrixSq<T, Len> doCholetskyDecomposition(MatrixSq<T, Len> matrix)
-        {
-            MatrixSq<T, Len> result;
-
-            // 1. Check for symmetric matrix
-
-            if (!isMatrixSymmetic(matrix))
-            {
-                // error: ABSOLUTELY wrong matrix
-                return result;
-            }
-
-            // 2. Check for positive-definite matrix
-
-            MatrixDefinition def = defineMatrix(matrix);
-            switch (def)
-            {
-            case MatrixDefinition::NegativeDefinite:     // error: how tf you did that (wrong matrix)?
-            case MatrixDefinition::NegativeSemidefinite: // error: how tf you did that (wrong matrix)?
-            case MatrixDefinition::Undefinite:           // error: how tf you did that (wrong matrix)?
-            case MatrixDefinition::PositiveSemidefinite: // error: not enough values
-            case MatrixDefinition::ZeroDefinite:         // error: empty matrix
-                return result;
-
-            case MatrixDefinition::PositiveDefinite: // ok
-                break;
-            }
-
-            // 3. Decomposition itself
-            for (size_t j = 0; j < matrix.size(); j++)
-            {
-                for (size_t i = 0; i < matrix.size(); i++)
-                {
-                    if ((i == 1) && (j == 1))
-                    {
-                        matrix[i][j] = std::sqrt<T>(matrix[i][i]);
-                        continue;
-                    }
-
-                    if (j == 1)
-                    {
-                        matrix[i][j] = matrix[i][j] / result[0][0];
-                        continue;
-                    }
-
-                    if (i == j)
-                    {
-                        matrix[i][j] = std::sqrt<T>(matrix[i][i] - sumOfSquaresUntil(result[i], i));
-                        continue;
-                    }
-
-                    matrix[i][j] = (matrix[i][j] - sumOfProductsUntil(result[i], result[j], i)) / result[i][i];
-                }
-            }
-
-            return result;
-        }
-    }
-
+    /**
+     * @brief Function to statistically calculate covariance matrix using statistic data
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam Len Matrix size
+     * @param input_vectors Input vectors to calculate covariance matrix
+     * @return MatrixSq<T, Len> The covariance matrix
+     */
     template <typename T, size_t Len>
-    valueVector<T, Len> generateMNV(MatrixSq<T, Len> covariance, valueVector<T, Len> meanVector, T seed)
-    {
-        static std::mt19937 generator{};
-        generator.seed(seed);
-        std::normal_distribution<T> distribution{0, 1};
+    MatrixSq<T, Len> calculateCovarianceMatrix(std::vector<valueVector<T, Len>> &input_vectors);
 
-        valueVector<T, Len> randomStandardNormalVector{};
-        for (size_t i = 0; i < randomStandardNormalVector.size(); i++)
-        {
-            randomStandardNormalVector[i] = distribution(generator);
-        }
-
-        MatrixSq<T, Len> decomposedMatrix = internal::doCholetskyDecomposition(covariance);
-        valueVector<T, Len> multipliedVector = internal::multiplyVectorByMatrix(randomStandardNormalVector, decomposedMatrix);
-        return internal::addVectors(multipliedVector, meanVector);
-    }
-
+    /**
+     * @brief Function to statistically calculate mean vector using statistic data
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam Len Vectors' size
+     * @param input_vectors Input vectors to calculate mean vector
+     * @return valueVector<T, Len> The mean vector
+     */
     template <typename T, size_t Len>
-    valueVector<T, Len> calculateMeanVector(std::vector<valueVector<T, Len>> &input_vectors)
-    {
-        valueVector<T, Len> result;
+    valueVector<T, Len> calculateMeanVector(std::vector<valueVector<T, Len>> &input_vectors);
 
-        for (auto &&vec : input_vectors)
-        {
-            result = internal::addVectors(result, vec);
-        }
-
-        for (size_t i = 0; i < result.size(); i++)
-        {
-            result[i] /= input_vectors.size()
-        }
-
-        return result;
-    }
-
+    /**
+     * @brief The main generator function for the multivariate normal random values (unfinished)
+     *
+     * @tparam T Underlying type, supposedly float/decimal
+     * @tparam Len Generated vectors' size
+     * @param covariance Covariance matrix of the distribution. Must be symmetrical and positive-definite
+     * @param meanVector Mean vector
+     * @param seed Generator seed. Internal rng state is always reset
+     * @return valueVector<T, Len> Generated multivariate normal value
+     */
     template <typename T, size_t Len>
-    MatrixSq<T, Len> calculateCovarianceMatrix(std::vector<valueVector<T, Len>> &input_vectors)
-    {
-        MatrixSq<T, Len> result;
-
-        valueVector<T, Len> mean = calculateMeanVector(input_vectors);
-
-        for (int i = 0; i < result.size(); i++)
-        {
-            for (int j = 0; j < result.size(); j++)
-            {
-                for (int k = 0; k < input_vectors.size(); k++)
-                {
-                    result[i][j] += (input_vectors[k][i] - mean[i]) * (input_vectors[k][j] - mean[j]);
-                }
-
-                result[i][j] /= input_vectors.size();
-            }
-        }
-
-        return result;
-    }
+    valueVector<T, Len> generateMNV(MatrixSq<T, Len> covariance, valueVector<T, Len> meanVector, T seed);
 
     int add(int a, int b);
 } // namespace mnv
 
-#endif // MNV HPP
+#endif // MNV_HPP
